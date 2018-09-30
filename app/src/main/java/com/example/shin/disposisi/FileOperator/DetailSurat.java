82d07c007bedda233ae93241c4102e10fbfaa6bc
nurufaisolrahman <nurufaisolrahman@gmail.com>
2018-09-30 11:58:00 +0700

    -Operator Download Lembar Disposisi Surat

--- app/src/main/java/com/example/shin/disposisi/FileOperator/DetailSurat.java
+++ app/src/main/java/com/example/shin/disposisi/FileOperator/DetailSurat.java
@@ -1,7 +1,13 @@
 package com.example.shin.disposisi.FileOperator;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.shin.disposisi.R;
@@ -9,6 +15,9 @@ import com.example.shin.disposisi.R;
 public class DetailSurat extends AppCompatActivity {
 
     EditText SuratDari,TanggalSurat,NomorSurat,DiterimaTanggal,NomorAgenda,Sifat,Perihal;
+    Button Cetak;
+    DownloadManager Download;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +30,7 @@ public class DetailSurat extends AppCompatActivity {
         NomorAgenda = findViewById(R.id.nomor_agenda);
         Sifat = findViewById(R.id.sifat);
         Perihal = findViewById(R.id.perihal);
+        Cetak = findViewById(R.id.Cetak);
 
         SuratDari.setText(" : "+RV_Adapter_Arsip_Operator.SuratDari);
         TanggalSurat.setText(" : "+RV_Adapter_Arsip_Operator.TanggalSurat);
@@ -29,5 +39,19 @@ public class DetailSurat extends AppCompatActivity {
         NomorAgenda.setText(" : "+RV_Adapter_Arsip_Operator.NomorAgenda);
         Sifat.setText(" : "+RV_Adapter_Arsip_Operator.Sifat);
         Perihal.setText(" : "+RV_Adapter_Arsip_Operator.Perihal);
+
+        Cetak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Download = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                Uri uri = Uri.parse("http://192.168.43.223/disposisi/CetakPDF.php?NomorSurat="+RV_Adapter_Arsip_Operator.NomorSurat);
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "Disposisi_"+RV_Adapter_Arsip_Operator.NomorSurat+".pdf");
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                Download.enqueue(request);
+            }
+        });
+
+
     }
 }
